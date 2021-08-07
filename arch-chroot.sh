268236db01aa3828e4f64e509a9cc0b1c91e64c0
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2021-08-07 16:05:55 -0600

    Add files via upload
    This script allows me to decypt, mount and chroot into my arch install on an other drive from Pop/Ubuntu

---
+++ arch-chroot.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+echo "----------------------------------------"
+echo "| Decrypting drive and using lvm magic |"
+echo "----------------------------------------"
+
+sudo cryptsetup luksOpen /dev/sdc2 crypt-root
+sudo lvscan
+sudo vgchange -ay
+
+echo "---------------------------------"
+echo "| Moving around and mounting up |"
+echo "---------------------------------"
+
+sudo mount /dev/mapper/crypt-root /mnt
+sudo mount /dev/sdc1 /mnt/boot
+sudo cp /etc/resolv.conf /mnt/etc/resolv.conf
+cd /mnt
+sudo mount -t proc /proc proc/
+sudo mount -t sysfs /sys sys/
+sudo mount --rbind /dev dev/
+
+echo "-----------------------"
+echo "| Entering the Matrix |"
+echo "-----------------------"
+
+sudo chroot /mnt /bin/bash
