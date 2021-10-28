d076fa51a2984a9401df5baa11e618ee5173ef72
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2021-10-28 09:30:37 -0600

    update pathes

--- update-git.sh
+++ update-git.sh
@@ -4,7 +4,7 @@ echo "------------------------"
 echo "Updating System76 Docs |"
 echo "------------------------"
 echo ""
-cd ~/Git/Github/docs; git pull; cd ..
+cd ~/Git/docs; git pull; cd ..
 
 echo ""
 echo "Done"
@@ -14,7 +14,7 @@ echo "-----------------------------"
 echo "Updating System76 Tech Docs |"
 echo "-----------------------------"
 echo ""
-cd ~/Git/Github/tech-docs; git pull; cd ..
+cd ~/Git/tech-docs; git pull; cd ..
 
 echo ""
 echo "Done"
@@ -24,38 +24,9 @@ echo "--------------------------"
 echo "Updating System76 Driver |"
 echo "--------------------------"
 echo ""
-cd ~/Git/Github/system76-driver; git pull; cd ..
+cd ~/Git/system76-driver; git pull; cd ..
 
 echo ""
 echo "Done"
 echo ""
 
-echo "-----------------"
-echo "Updating JOSHUA |"
-echo "-----------------"
-echo ""
-cd ~/Git/Github/joshua; git pull; cd ..
-
-echo ""
-echo "Done"
-echo ""
-
-echo "-----------------"
-echo "Updating Artoo  |"
-echo "-----------------"
-echo ""
-cd ~/Git/Github/artoo; git pull; cd ..
-
-echo ""
-echo "Done"
-echo ""
-
-echo "-----------------"
-echo "Updating Assets |"
-echo "-----------------"
-echo ""
-cd ~/Git/Github/assets; git pull; cd ..
-
-echo ""
-echo "Done"
-echo ""
