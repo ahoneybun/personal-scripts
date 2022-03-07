4f57431a0267bd972e40a24a96cd2f1d72b6286b
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2022-03-07 07:10:52 -0700

    Update update-git.sh

--- update-git.sh
+++ update-git.sh
@@ -4,7 +4,7 @@ echo "------------------------"
 echo "Updating System76 Docs |"
 echo "------------------------"
 echo ""
-cd ~/Git/docs; git config pull.rebase false; git pull; cd ..
+cd ~/Projects/system76/docs; git pull; cd ..
 
 echo ""
 echo "Done"
@@ -14,7 +14,17 @@ echo "-----------------------------"
 echo "Updating System76 Tech Docs |"
 echo "-----------------------------"
 echo ""
-cd ~/Git/tech-docs; git config pull.rebase false; git pull; cd ..
+cd ~/Projects/system76/tech-docs; git pull; cd .
+
+echo ""
+echo "Done"
+echo ""
+
+echo "-------------------"
+echo "Updating Careteam |"
+echo "-------------------"
+echo ""
+cd ~/Projects/system76/careteam; git pull; cd ..
 
 echo ""
 echo "Done"
@@ -24,28 +34,9 @@ echo "--------------------------"
 echo "Updating System76 Driver |"
 echo "--------------------------"
 echo ""
-cd ~/Git/system76-driver; git config pull.rebase false; git pull; cd ..
+cd ~/Projects/system76/system76-driver; git pull; cd ..
 
 echo ""
 echo "Done"
 echo ""
 
-echo "-------------------"
-echo "Updating Firmware |"
-echo "-------------------"
-echo ""
-cd ~/Git/firmware; git config pull.rebase false; git pull; cd ..
-
-echo ""
-echo "Done"
-echo ""
-
-echo "-----------------------"
-echo "Updating HiDPI Daemon |"
-echo "-----------------------"
-echo ""
-cd ~/Git/hidpi-daemon; git config pull.rebase false; git pull; cd ..
-
-echo ""
-echo "Done"
-echo ""
