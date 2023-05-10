a12344f8d0161189643f1f7bbc70a4d628b9e6d7
Aaron Honeycutt <aaronhoneycutt@proton.me>
2023-05-10 10:44:29 -0600

    Add RAM logic

--- pang12-test.sh
+++ pang12-test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+ramTotal=$(free -h | awk '/^Mem:/{print $2}'| awk -FG {'print$1'})
+ramTotal=($((ramTotal-4)))
+echo $ramTotal
+
 # Checking if the tools are installed"
 if ! [ -x "$(command -v stress-ng)" ];
 then
@@ -12,5 +16,5 @@ then
 fi
 
 gnome-terminal -- glmark2 --run-forever
-gnome-terminal -- stress-ng -m 0 -c 0 --vm-bytes 25G
+gnome-terminal -- stress-ng -m 0 -c 0 --vm-bytes "$ramTotal"G
 gnome-terminal -- journalctl -f | grep -i error
