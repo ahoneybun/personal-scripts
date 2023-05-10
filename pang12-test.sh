d16e1d7d7c248a583591004a9e3512e8cbbaacd2
Aaron Honeycutt <aaronhoneycutt@proton.me>
2023-05-10 08:16:29 -0600

    Fix logic

--- pang12-test.sh
+++ pang12-test.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 # Checking if the tools are installed"
-if ! [ -x "$(command -v stress-ng glmark2)" ];
+if ! [ -x "$(command -v stress-ng)" ];
 then
-  sudo apt install stress-ng glmark2
-  exit
+  sudo apt install stress-ng
+fi
+
+if ! [ -x "$(command -v glmark2)" ];
+then 
+  sudo apt install glmark2 
 fi
 
 gnome-terminal -- glmark2 --run-forever
-gnome-terminal -- stress-ng -m 0 -c 0 --vm-bytes 25G
+#gnome-terminal -- stress-ng -m 0 -c 0 --vm-bytes 25G
 gnome-terminal -- journalctl -f | grep -i error
