a9872e9a33f060c9dbc401e035eac0f9e55d0883
Aaron Honeycutt <aaronhoneycutt@proton.me>
2023-05-10 08:16:49 -0600

    Uncomment command

--- pang12-test.sh
+++ pang12-test.sh
@@ -12,5 +12,5 @@ then
 fi
 
 gnome-terminal -- glmark2 --run-forever
-#gnome-terminal -- stress-ng -m 0 -c 0 --vm-bytes 25G
+gnome-terminal -- stress-ng -m 0 -c 0 --vm-bytes 25G
 gnome-terminal -- journalctl -f | grep -i error
