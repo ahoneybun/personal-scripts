707ff4f3f7ffe27418fb46e817f2a3942d755766
Aaron Honeycutt <aaronhoneycutt@proton.me>
2023-05-10 07:33:28 -0600

    Add installing packages if they are missing

--- pang12-test.sh
+++ pang12-test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Installing tools if they are missing"
+echo ""
+sudo apt install stress-ng glmark2
+
 gnome-terminal -- glmark2 --run-forever
 gnome-terminal -- stress-ng -m 0 -c 0 --vm-bytes 25G
 gnome-terminal -- journalctl -f | grep -i error
