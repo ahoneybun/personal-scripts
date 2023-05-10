899f3a9a813fc01a68c7058fd38c577b999eb376
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2023-05-10 08:11:21 -0600

    Update pang12-test.sh

--- pang12-test.sh
+++ pang12-test.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Installing tools if they are missing"
-echo ""
-sudo apt install stress-ng glmark2
+# Checking if the tools are installed"
+if ! [ -x "$(command -v stress-ng glmark2)" ];
+then
+  sudo apt install stress-ng glmark2
+  exit
+fi
 
 gnome-terminal -- glmark2 --run-forever
 gnome-terminal -- stress-ng -m 0 -c 0 --vm-bytes 25G
