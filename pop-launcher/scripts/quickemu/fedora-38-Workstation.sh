f1501623bcc3c9dfa0c4ade9e5ec7dea9eadb647
Aaron Honeycutt <aaronhoneycutt@proton.me>
2023-07-12 12:59:44 -0600

    Add pop-launcher scripts for VMs

---
+++ pop-launcher/scripts/quickemu/fedora-38-Workstation.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# name: Start Fedora 38 VM
+# icon: /home/aaronh/Pictures/distro-icons/fedora-logo.png
+# description: Workstation Edition
+# keywords: fedora 38 workstation
+
+cd /mnt/VM && quickemu --vm fedora-38-Workstation.conf
+
---
+++ pop-launcher/scripts/quickemu/ubuntu-22.04.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# name: Start Ubuntu 22.04 VM
+# icon: preferences-system-symbolic
+# description: Starts the Ubuntu 22.04 VM
+# keywords: ubuntu 22.04
+
+cd /mnt/VM && quickemu --vm ubuntu-22.04.conf
+
