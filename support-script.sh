45446f4bfbed2cdf9e14add68490a076f13b12c1
Aaron Honeycutt <aaronhoneycutt@proton.me>
2025-01-07 10:47:36 -0700

    start work on support script

--- support-script.sh
+++ support-script.sh
@@ -3,16 +3,21 @@
 echo "------------------------------------------------------"
 echo "Hello $USER what would you like to do today?"
 echo "------------------------------------------------------"
+echo ""
 
 echo "[1] Clear EFI entries from firmware"
+echo "[2] Fix package manager"
 echo ""
 
 echo "------------------------------------------------------"
 echo -n "Enter choice: "; read choice
 case "$choice" in
 
-   1) for i in $(seq 0 9); do sudo efibootmgr -B -b 000$i 2>/dev/null; done
-      sudo bootctl --path=/boot/efi install
-   ;;
+    1) for i in $(seq 0 9); do sudo efibootmgr -B -b 000$i 2>/dev/null; done
+       sudo bootctl --path=/boot/efi install
+    ;;
+
+    2) sh support-backend/apt/main.sh
+    ;;
 
 esac
