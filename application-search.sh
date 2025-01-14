46b930778b0880d6d3ddacf8a4ce41e7fc0d08c2
Aaron Honeycutt <aaronhoneycutt@proton.me>
2025-01-14 08:33:37 -0700

    remove case loop and mark as exec

--- application-search.sh
+++ application-search.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-echo ""
-echo "[1] Find if an application is installed with flatpak or apt"
-echo ""
+   echo ""
+   echo -n "What is the application name? "; read application
+   echo "Searching for '$application'..."
 
-echo "------------------------------------------------------"
-echo -n "Enter choice: "; read choice
-case "$choice" in
+       # Run flatpak list and grep for the application
+       flatpak list | grep "$application"
 
-    1)
-        echo ""
-        echo -n "What is the application name? "; read application
-        echo "Searching for '$application'..."
-
-        # Run flatpak list and grep for the application
-        flatpak list | grep "$application"
-
-        # Run apt search for the application
-        apt list --installed | grep "$application" 2>/dev/null
-    ;;
-
-esac
+       # Run apt search for the application
+       apt list --installed | grep "$application" 2>/dev/null
