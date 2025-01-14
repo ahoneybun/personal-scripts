ae8246dc4d6dd205200708b35af560c0a4d76bc0
Aaron Honeycutt <aaronhoneycutt@proton.me>
2025-01-14 09:39:09 -0700

    send apt warning to /dev/null and add check for applications in /usr/bin

--- application-search.sh
+++ application-search.sh
@@ -5,7 +5,19 @@
    echo "Searching for '$application'..."
 
        # Run flatpak list and grep for the application
+       echo ""
+       echo "Checking if it is a Flatpak application"
+       echo ""
        flatpak list | grep "$application"
 
        # Run apt search for the application
-       apt list --installed | grep "$application" 2>/dev/null
+       echo ""
+       echo "Checking if it is a Debian application"
+       echo ""
+       apt list --installed 2>/dev/null | grep "$application"
+
+       # Run ls | grep search for the application in /usr/bin
+       echo ""
+       echo "Checking if it is installed in /usr/bin"
+       echo ""
+       ls /usr/bin | grep "$application" 
