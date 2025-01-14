1583cfe77ad003dca98a46f5718e9e626415b9a1
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2025-01-14 08:29:42 -0700

    Create application-search.sh

---
+++ application-search.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo ""
+echo "Common Tasks"
+echo ""
+echo "[1] Find if an application is installed with flatpak or apt"
+echo ""
+
+echo "------------------------------------------------------"
+echo -n "Enter choice: "; read choice
+case "$choice" in
+
+    1)
+        echo ""
+        echo -n "What is the application name? "; read application
+        echo "Searching for '$application'..."
+
+        # Run flatpak list and grep for the application
+        flatpak list | grep "$application"
+
+        # Run apt search for the application
+        apt list --installed | grep "$application" 2>/dev/null
+    ;;
+
+esac
